7c31373b98d2b77fb35d6c4e0e0a312bf8c54afc
ervina <eindivinereinherjar@gmail.com>
2018-12-10 16:11:07 +0700

    add structur JSON buat feedback

---
+++ src/main/java/com/spring/miniproject/service/FeedbackService.java
@@ -0,0 +1,9 @@
+package com.spring.miniproject.service;
+
+import com.spring.miniproject.model.FeedbackModel;
+
+public interface FeedbackService {
+	
+	public FeedbackModel create(FeedbackModel feedbackModel);
+	
+}
